527a964906ead95cb446c626f8d8bdd25faefc0b
MukundMurarka1 <mk847225@gmail.com>
2023-03-16 19:51:17 +0530

    revision4

---
+++ eclipse java code practice/SecondServlet_Revision/ThirdServlet.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import javax.servlet.*;
+public class ThirdServlet implements Servlet{
+
+    static 
+    {
+        System.out.println("Third Servlet .class file is loading .......");
+    }
+
+    public ThirdServlet()
+    {
+        System.out.println("Third Servlet object is instantiated .......");
+    }
+
+    public void init(ServletConfig config) throws ServletException
+    {
+        System.out.println("Second servlet is initilized successfull ..... ");
+    }
+
+    public ServletConfig getServletConfig()
+    {
+        return null;
+
+    }
+
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException 
+    {
+        response.setContentType("text/html");  
+        System.out.println("Servlet request processing logic is starting ......");
+
+        PrintWriter write = response.getWriter();
+
+        write.println("<html><head><title>OUTPUT</title></head>");
+        write.println("<body>");
+        write.println("<h1 style='color:red'><marquee> WELCOME TO ADVANCE JAVA COURSE </marquee></h1>");
+        write.println("</body>");
+        write.println("</html>");
+
+        write.close();
+    }
+
+
+    public String getServletInfo()
+    {
+        return null;
+
+    }
+    public void destroy(){
+        System.out.println("Servlet logic is Successfully executed Successfully ");
+    }
+
+
+}
+
