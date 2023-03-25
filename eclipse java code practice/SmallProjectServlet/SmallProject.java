72cab8e73ae77de040e25d5627e26d7e0a950c6d
MukundMurarka1 <mk847225@gmail.com>
2023-03-25 21:16:49 +0530

    some changes

---
+++ eclipse java code practice/SmallProjectServlet/SmallProject.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(urlPatterns="/reg")
+public class SmallProject extends HttpServlet{
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException
+    {
+        
+    }
+
+
+
+    
+}
