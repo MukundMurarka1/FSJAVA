2590e88c4734af3831890351205019bf527a929e
MukundMurarka1 <mk847225@gmail.com>
2023-03-31 21:09:30 +0530

    data

--- eclipse java code practice/ServletDBCommunication/src/main/java/in/ineuron/controller/ServletDataBase.java
+++ eclipse java code practice/ServletDBCommunication/src/main/java/in/ineuron/controller/ServletDataBase.java
@@ -2,13 +2,17 @@ package in.ineuron.controller;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 
 
 public class ServletDataBase extends HttpServlet {
@@ -52,6 +56,37 @@ public class ServletDataBase extends HttpServlet {
        
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		ResultSet resultset = null;
+		
+		String query = "select * from studentdetails";
+		
+		try {
+			Statement stmt = connection.createStatement();
+			
+			System.out.println("Statement created successfully ........");
+			
+			resultset = stmt.executeQuery(query);
+			
+			System.out.println("First_Name/Last_name/Roll_no/Address/Mobile_number");
+			
+			while(resultset.next()) {
+				
+				String firstname= resultset.getString(1);
+				String lastname = resultset.getString(2);
+				String rollno = resultset.getString(3);
+				String address = resultset.getString(4);
+				String mobileno = resultset.getString(5);
+				
+				System.out.println(firstname+ "/t" +lastname+ "/t" +rollno+ "/t" +address+ "/t" + mobileno);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 	}
 
