9e85897199e8050b86be368a3bd8c70e306f43dd
MukundMurarka1 <mk847225@gmail.com>
2023-04-03 18:01:40 +0530

    context

--- eclipse java code practice/ServletContextApplication/src/main/java/in/ineuron/controller/TestServletContext.java
+++ eclipse java code practice/ServletContextApplication/src/main/java/in/ineuron/controller/TestServletContext.java
@@ -1,6 +1,10 @@
 package in.ineuron.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +16,37 @@ public class TestServletContext extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter write = response.getWriter();
 		
+		write.println("<html><head><title>Context param</title></head>");
+		write.println("<body>");
+		write.println("<center>");
+		write.println("<h1 style='color:green; text-align:center'>welcome to this database information page </h1>");
+		write.println("<table>");
+		write.println("<th> Context Name </th><th> Context Value </th>");
+		
+		ServletContext context = getServletContext();
+		Enumeration<String> parameternames = context.getInitParameterNames();
+		
+		while(parameternames.hasMoreElements()) {
+			String parametername = (String) parameternames.nextElement();
+			String parametervalue = context.getInitParameter(parametername);
+			
+			write.println("<tr>");
+			write.println("<td>" +parametername+ "</td><td>" +parametervalue+ "</td>");
+			
+			write.println("</tr>");
+		}
+		
+		write.println("</table>");
+		write.println("</center>");
+		write.println("</body>");
+		write.println("</html>");
+		
+		
+		
+		
+		write.close();
 	}
 
 }
