a588e730551da2750d324c736e74380b7eb65ccd
MukundMurarka1 <mk847225@gmail.com>
2023-04-04 20:00:36 +0530

    new

---
+++ eclipse java code practice/ProjectApp/src/main/java/in/ineuron/controller/NewPorject.java
@@ -0,0 +1,24 @@
+package in.ineuron.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NewPorject extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		ServletConfig config = getServletConfig();
+		ServletContext context = getServletContext();
+	}
+
+}
