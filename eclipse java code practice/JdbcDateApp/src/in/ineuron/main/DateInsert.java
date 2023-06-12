994d9c568901dc78f0cec233374d71da8ea59b86
MukundMurarka1 <mk847225@gmail.com>
2023-06-12 09:27:48 +0530

    dateapp

--- eclipse java code practice/JdbcDateApp/src/in/ineuron/main/DateInsert.java
+++ eclipse java code practice/JdbcDateApp/src/in/ineuron/main/DateInsert.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import in.ineuron.util.JdbcUtil;
 
@@ -21,6 +22,18 @@ public class DateInsert {
 			System.out.println("Connection established successfully ....");
 			if(connection  != null) {
 				pstmt = connection.prepareStatement(null);
+			}
+			if(pstmt !=null ) {
+				Scanner scan = new Scanner(System.in);
+				System.out.print("enter the name of student :: ");
+				String sname =  scan.next();
+				System.out.print("enter the Date of birth of student ::" );
+				String sdob = scan.next();
+				System.out.print("enter date of marriage of student :: ");
+				String sdom = scan.next();
+				
+				//converison of date into 
+				
 			}
 			
 		}catch(SQLException se) {
