a24ee47437a1977aa40af91a42e322d1ad132868
MukundMurarka1 <mk847225@gmail.com>
2023-06-15 09:02:35 +0530

    image

--- eclipse java code practice/JdbcDateApp/src/in/ineuron/main/DateInsert.java
+++ eclipse java code practice/JdbcDateApp/src/in/ineuron/main/DateInsert.java
@@ -1,13 +1,13 @@
 package in.ineuron.main;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import in.ineuron.util.JdbcUtil;
@@ -23,37 +23,54 @@ public class DateInsert {
 		java.sql.Date stdom = null;
 		
 		String insertquery = "insert into user(`sname`,`sdob`,`sdom`) values (?,?,?);";
+		String imageInsertQuery = "insert into information (`sname`,`image`) values(?,?)";
 		
 		try {
 			connection = JdbcUtil.jdbcgetconnection();
 			System.out.println("Connection established successfully ....");
 			if(connection  != null) {
-				pstmt = connection.prepareStatement(insertquery);
+				pstmt = connection.prepareStatement(imageInsertQuery);
 			}
 			if(pstmt != null ) {
+//				Scanner scan = new Scanner(System.in);
+//				System.out.print("enter the name of student :: ");
+//				String sname =  scan.next();
+//				System.out.print("enter the Date of birth of student in dd-MM-yyyy format ::" );
+//				String sdob = scan.next();
+//				System.out.print("enter date of marriage of student in yyyy-MM-dd format :: ");
+//				String sdom = scan.next();
+//				
+//				//converison of date into java.util.date
+//				if(sdob != null) {
+//				SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+//				java.util.Date sttdob = sdf.parse(sdom);
+//				
+//				long datevalue = sttdob.getTime();
+//				 stdob = new java.sql.Date(datevalue);
+//				}
+//				if(sdom !=null) {
+//				//conversion of date into java.sql.date
+//				stdom = java.sql.Date.valueOf(sdom);	
+//				}
+//				pstmt.setString(1, sname);
+//				pstmt.setDate(2,stdob);
+//				pstmt.setDate(3, stdom);
+				
+				
+				//image insertion code 
+				
 				Scanner scan = new Scanner(System.in);
-				System.out.print("enter the name of student :: ");
-				String sname =  scan.next();
-				System.out.print("enter the Date of birth of student in dd-MM-yyyy format ::" );
-				String sdob = scan.next();
-				System.out.print("enter date of marriage of student in yyyy-MM-dd format :: ");
-				String sdom = scan.next();
+				System.out.println("enter the name of student :: ");
+				String sname = scan.next();
 				
-				//converison of date into java.util.date
-				if(sdob != null) {
-				SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-				java.util.Date sttdob = sdf.parse(sdom);
+				System.out.println("Enter the loction of image");
+				String imgloc = scan.next();
 				
-				long datevalue = sttdob.getTime();
-				 stdob = new java.sql.Date(datevalue);
-				}
-				if(sdom !=null) {
-				//conversion of date into java.sql.date
-				stdom = java.sql.Date.valueOf(sdom);	
-				}
+				FileInputStream fis = new FileInputStream(new File(imgloc));
+				
 				pstmt.setString(1, sname);
-				pstmt.setDate(2,stdob);
-				pstmt.setDate(3, stdom);
+				pstmt.setBinaryStream(2, fis);
+				
 				
 				int rowaffected = pstmt.executeUpdate();
 				if(rowaffected != 0) {
@@ -67,8 +84,6 @@ public class DateInsert {
 			
 		}catch(IOException ie ) {
 			ie.printStackTrace();
-		}catch(ParseException pe) {
-			pe.printStackTrace();
 		}finally {
 			try{
 				JdbcUtil.closeConnection(connection, pstmt, resultset);
