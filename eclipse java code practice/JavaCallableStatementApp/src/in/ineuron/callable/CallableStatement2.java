2a5fcf405d7effda1fc0cdba70f53398b2b63d2b
MukundMurarka1 <mk847225@gmail.com>
2023-06-27 09:48:05 +0530

    cstsmt

---
+++ eclipse java code practice/JavaCallableStatementApp/src/in/ineuron/callable/CallableStatement2.java
@@ -0,0 +1,17 @@
+package in.ineuron.callable;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+public class CallableStatement2 {
+	
+	Connection connection = null;
+	CallableStatement cstmt = null;
+	ResultSet resultset = null;
+	
+	String prod1 = null;
+	String prod2 = null;
+	
+
+}
