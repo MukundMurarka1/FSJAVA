b7aaf759fff9d6e06d626d8dd5dbae5bc1eddd20
MukundMurarka1 <mk847225@gmail.com>
2023-07-16 13:46:47 +0530

    update complete

--- eclipse java code practice/JDBCUpdatebleApp/src/in/ineuron/Updateble/updatebleApp.java
+++ eclipse java code practice/JDBCUpdatebleApp/src/in/ineuron/Updateble/updatebleApp.java
@@ -33,10 +33,24 @@ public class updatebleApp {
 		}
 		if(resultset != null) {
 			System.out.println("Record before doing update operation ");
-			System.out.println();
+			System.out.println("id\tName\tAge\tAddress");
 			while (resultset.next()) {
-				System.out.println(resultset.getInt(1)+" \t "+);
+				System.out.println(resultset.getInt(1)+" \t "+resultset.getString(2)+"\t"+ resultset.getInt(3)+"\t"+resultset.getString(4));
 			}
+			
+			System.out.println();
+			System.out.println("Application is in pausing State :: please press enter key for upadte the record ::::");
+			System.in.read();
+			System.out.println("Record after updation ::: ");
+			
+			resultset.beforeFirst();//tecke the cursor before first record 
+			System.out.println("id\tName\tAge\tAddress");
+			while (resultset.next()) {
+				resultset.refreshRow();
+				System.out.println(resultset.getInt(1)+" \t "+resultset.getString(2)+"\t"+ resultset.getInt(3)+"\t"+resultset.getString(4));
+			}
+			
+			
 		}
 		
 	} catch (IOException | SQLException e) {
